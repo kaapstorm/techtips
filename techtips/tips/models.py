81e82dd02f98e996daed2d77c14d6091f1c18ecd
Norman Hooper <norman@kaapstorm.com>
2011-09-22 23:29:20 +0200

    slug bug fixed

--- techtips/tips/models.py
+++ techtips/tips/models.py
@@ -14,6 +14,7 @@
 # along with Tech Tip of the Day.  If not, see <http://www.gnu.org/licenses/>.
 
 import markdown
+import re
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -49,10 +50,17 @@ class Tip(models.Model):
                                          safe_mode='escape')
         # Check slug is unique
         if self.slug is None or len(self.slug) == 0:
+            # self.slug will be None if the tip was created using TipForm
             self.slug = slugify(self.title)
-        initial_slug = self.slug
+        if len(self.slug) == 0:
+            # If the title has no alphanumeric characters, slug will be empty
+            self.slug = 'x'
+        # Strip off any final digits
+        match = re.match('(.+?)(?:-\d+)?$', self.slug)
+        initial_slug = match.groups()[0]
         serial = 1
-        while Tip.objects.filter(slug=self.slug).count() > 0:
+        while Tip.objects.exclude(id=self.id).filter(slug=self.slug)\
+                         .count() > 0:
             serial += 1
             self.slug = '%s-%s' % (initial_slug, serial)
         super(Tip, self).save(*args, **kwargs)
